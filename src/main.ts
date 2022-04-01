347cdadae22d01acdf60fee2eb8fc56eb8a8aeb6
Yasin Khan <yasinkhan500@gmail.com>
2022-03-31 19:00:19 -0700

    stache

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app
+    .listen(PORT)
+    .then(() => {
+      console.log(`Server running on http://localhost:${PORT} 🚀`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 bootstrap();
